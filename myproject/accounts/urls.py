f08b034a9ccddef919e5b7eb22069d70ceb35555
AYAZ2006 <hajrasultana7075@gmail.com>
2025-03-20 08:10:54 +0530

    Update urls.py

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (RegisterView, LoginView,get_csrf_token,SearchUsersView,Send,View,AcceptRejectFriendRequestView,ListFriends)
+from .views import (RegisterView, LoginView,get_csrf_token,SearchUsersView,Send,View,AcceptRejectFriendRequestView,ListFriends,LoggedInUserView)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path("received-requests/<str:username>/", View.as_view(), name="friend-requests"),
     path("friend-requests/<str:username>/<str:action>/", AcceptRejectFriendRequestView.as_view(), name="accept-reject-friend-request"),
     path('accepted/<str:username>/', ListFriends.as_view(), name="accepted-friends"),
+    path('logged-in-user/', LoggedInUserView.as_view(), name='logged-in-user'),
 ]
