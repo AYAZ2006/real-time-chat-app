d17840d5022710e1d49a71898ed939c96abd8669
AYAZ2006 <hajrasultana7075@gmail.com>
2025-03-21 14:13:37 +0530

    Update models.py

--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -15,3 +15,14 @@ class FriendRequest(models.Model):
     class Meta:
         ordering=("timestamp",)
 
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_messages")
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="received_messages")
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["timestamp"]
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver}: {self.message[:50]}"
